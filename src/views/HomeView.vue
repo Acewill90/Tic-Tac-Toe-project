e999c28ad248fdfaae1920d276654b0b839a361e
Acewill90 <attila.kovacs.90@gmail.com>
2023-03-24 17:12:23 +0100

    feat: the game logic (only works with a first row)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="homepage">
     <div class="playground-grid">
-      <span v-for="i in 162" class="playground-grid--elem"></span>
+      <span v-for="i in 162" class="playground-grid--elem" @click="clickOnSquare(i, $event)"></span>
       <div class="welcome-screen" v-if="!readyForPlay">
         <div class="form-grid-section">
           <!-- 1st player -->
@@ -65,9 +65,16 @@
     name: "Home",
     data() {
       return {
-        playerOne: {name: '', avatar: ''},
+        playerOne: {name: '', avatar: '', mark: 'X'},
         playerTwo: {name: '', avatar: ''},
-        readyForPlay: false
+        readyForPlay: false,
+        boardForX: new Array(162),
+        boardForO: new Array(162),
+        startingX: true,
+        winningPattern: [
+          [1,2,3,4,5],[2,3,4,5,6],[3,4,5,6,7],[4,5,6,7,8],[5,6,7,8,9],[6,7,8,9,10],[7,8,9,10,11],[8,9,10,11,12],
+          [9,10,11,12,13],[10,11,12,13,14],[11,12,13,14,15],[12,13,14,15,16],[13,14,15,16,17],[14,15,16,17,18],
+        ]
       }
     },
     methods: {
@@ -99,11 +106,45 @@
       },
 
       resetPlayers(){
-          this.playerOne.name = '';
-          this.playerOne.avatar = '';
-          this.playerTwo.name = '';
-          this.playerTwo.avatar = '';
-          this.readyForPlay = false;
+        this.playerOne.name = '';
+        this.playerOne.avatar = '';
+        this.playerTwo.name = '';
+        this.playerTwo.avatar = '';
+        this.readyForPlay = false;
+      },
+
+      clickOnSquare(position, e){
+        // const arr1 = [ 1, 2, 10];
+        // const arr2 = [ 3, 5, 4, 2, 7, 0, 1, 10, undefined, 'reserved' ];
+
+        // const hasAllElems = arr1.every(elem => arr2.includes(elem));
+
+        // console.log(hasAllElems);
+
+        
+        if (this.startingX) {
+          if (this.boardForX[position-1] === undefined) { 
+            this.boardForX[position-1] = position;
+            this.boardForO[position-1] = 'reserved';
+            this.startingX = false;
+            e.target.classList.add('playground-grid--elem-active', 'playground-grid--elem-active-x');
+            for (let i = 0; i < this.winningPattern.length; i++){
+              const isWinner = this.winningPattern[i].every(element => this.boardForX.includes(element));
+              if (isWinner){
+                this.winningPattern[i].forEach(position => 
+                  document.querySelector('.playground-grid--elem:nth-child('+position+')').classList.add('playground-grid--elem-winner')
+                )  
+              } 
+            }
+          }          
+        } else {
+          if (this.boardForO[position-1] === undefined) {
+            this.boardForO[position-1] = position;
+            this.boardForX[position-1] = 'reserved';
+            this.startingX = true;
+            e.target.classList.add('playground-grid--elem-active', 'playground-grid--elem-active-o');
+          }  
+        }
       }
     },
     created (){
