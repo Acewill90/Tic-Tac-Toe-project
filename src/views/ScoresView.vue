23c14be6a75816208bc119c69a54fe59c5385549
Acewill90 <attila.kovacs.90@gmail.com>
2023-03-25 21:21:04 +0100

    feat: fetch scores from API

---
+++ src/views/ScoresView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>This is a score page</h1>
+</template>
+
+<script>
+export default {
+  name: "Top scores",
+  data() {
+    return {
+      winners: []
+    }
+  },
+
+  mounted() {    
+    const authToken = 'hkew57zhne345hb3kw-zh65u';
+
+    fetch('https://eomxihgqom5ex61.m.pipedream.net/high-scores', {
+      headers: {
+        'Authorization': `Bearer ${authToken}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => this.winners = data)
+    .catch(err => console.log(err.message))
+
+  }
+}
+</script>
