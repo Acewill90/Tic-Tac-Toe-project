061818f9f16903bcd7eaf5df475e778d46b63373
Acewill90 <attila.kovacs.90@gmail.com>
2023-03-26 23:26:27 +0200

    feat: displaying the score list

--- src/views/ScoresView.vue
+++ src/views/ScoresView.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>This is a score page</h1>
+  <main class="homepage">
+    <div class="playground-grid">
+      <span v-for="i in 162" class="playground-grid--elem"></span>
+
+      <div class="cover-screen cover-screen--scores">
+        <h2 class="h1">Toplista</h2>
+        <div class="list">
+          <div class="list--box" v-for="winner in winners.scores">
+            <span class="list--elem">name: "{{ winner.name }}",</span>
+            <span class="list--elem">score: {{ winner.score }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -26,3 +40,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  @import "../assets/scss/compiled/scores.min.css";
+</style>
