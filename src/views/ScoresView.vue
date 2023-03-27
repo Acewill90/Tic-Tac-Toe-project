97bafd0578db04c5ec98124edb35c3b607559893
Acewill90 <attila.kovacs.90@gmail.com>
2023-03-27 18:33:37 +0200

    feat: new grid background on Scores page

--- src/views/ScoresView.vue
+++ src/views/ScoresView.vue
@@ -1,24 +1,29 @@
 <template>
   <main class="homepage">
-    <div class="playground-grid">
-      <span v-for="i in 162" class="playground-grid--elem"></span>
-
-      <div class="cover-screen cover-screen--scores">
-        <h2 class="h1">Toplista</h2>
-        <div class="list">
-          <div class="list--box" v-for="winner in winners.scores">
-            <span class="list--elem">name: "{{ winner.name }}",</span>
-            <span class="list--elem">score: {{ winner.score }}</span>
-          </div>
+    <div class="scores-wrapper">
+      <div class="scores-wrapper--bckg">
+        <span v-for="i in 300" class="scores-wrapper--elem"></span>
+      </div>
+      <h2 class="h1">Toplista</h2>
+      <div class="list">
+        <div class="list--box" v-for="winner in winners.scores">
+          <span class="list--elem">name: "{{ winner.name }}",</span>
+          <span class="list--elem">score: {{ winner.score }}</span>
         </div>
       </div>
     </div>
+    <MobileFooter />
   </main>
 </template>
 
 <script>
+import MobileFooter from '../components/MobileFooter.vue'
+
 export default {
   name: "Top scores",
+  components: {
+    MobileFooter
+  },
   data() {
     return {
       winners: []
@@ -37,6 +42,10 @@ export default {
     .then(data => this.winners = data)
     .catch(err => console.log(err.message))
 
+  },
+
+  created() {
+    this.emitter.emit('onScoresPage');
   }
 }
 </script>
